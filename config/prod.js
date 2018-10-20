b0dbaffea14cabc03f33e8e063652e35789cc229
Chris Ellis <ellis.cs@gmail.com>
2018-10-20 05:47:07 -0400

    Finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+// vj2Tyb8nUPfQdz7c
+// 94017234606-46nbsvr4dcq2gh4f03a1nj4ej8bp67sf.apps.googleusercontent.com
+// lvY_Kyn9PwokBXGQxAK0t_Rx
