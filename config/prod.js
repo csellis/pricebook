f5e155a180f0ee0f73084c9849a5fcb6a26dc102
Chris Ellis <ellis.cs@gmail.com>
2018-10-20 05:55:59 -0400

    tweak google strategy settings

--- config/prod.js
+++ config/prod.js
@@ -4,7 +4,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-
-// vj2Tyb8nUPfQdz7c
-// 94017234606-46nbsvr4dcq2gh4f03a1nj4ej8bp67sf.apps.googleusercontent.com
-// lvY_Kyn9PwokBXGQxAK0t_Rx
