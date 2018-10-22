8dbb96f47d4399c53f25d2180dbf9903c63b9233
Chris Ellis <ellis.cs@gmail.com>
2018-10-22 06:01:40 -0400

    Add client & auth

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Price Book</h1>
+      <h4>
+        Log prices of the stuff
+        <br /> you care about
+      </h4>
+    </div>
+  );
+};
+
+export default Landing;
